fca7a5e5093d6d9597fd1da73a9cda7ee75f21e7
jinscoding <hojin9901@naver.com>
2024-08-05 15:48:53 +0900

    [Fix/#13] AlarmButton 수정

--- src/components/mypage/AlarmButton.tsx
+++ src/components/mypage/AlarmButton.tsx
@@ -9,7 +9,7 @@ interface AlarmButtonProps {
 
 function AlarmButton({ alarm, onClick, children }: AlarmButtonProps) {
   return (
-    <AlarmButtonStyle alarm={alarm} onClick={onClick}>
+    <AlarmButtonStyle $alarm={alarm} onClick={onClick}>
       {children}
     </AlarmButtonStyle>
   );
@@ -17,13 +17,13 @@ function AlarmButton({ alarm, onClick, children }: AlarmButtonProps) {
 
 export default AlarmButton;
 
-const AlarmButtonStyle = styled.button<{ alarm: boolean }>`
+const AlarmButtonStyle = styled.button<{ $alarm: boolean }>`
   width: 75px;
   height: 32px;
-  background-color: ${({ theme, alarm }) =>
-    alarm ? theme.color_key : theme.color_bgLightOrange};
-  color: ${({ theme, alarm }) =>
-    alarm ? theme.color_textLightOrange : theme.color_key};
+  background-color: ${({ theme, $alarm }) =>
+    $alarm ? theme.color_key : theme.color_bgLightOrange};
+  color: ${({ theme, $alarm }) =>
+    $alarm ? theme.color_textLightOrange : theme.color_key};
   font-size: ${({ theme }) => theme.fontSize_xs};
   border: none;
   border-radius: 5px;
