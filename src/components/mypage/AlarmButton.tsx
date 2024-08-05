06b91b0b2177367a2410ccd4ca44c0bdc5566ba8
jinscoding <hojin9901@naver.com>
2024-08-05 11:42:01 +0900

    [Style/#13] AlarmButton 컴포넌트 스타일링

---
+++ src/components/mypage/AlarmButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface AlarmButtonProps {
+  alarm: boolean;
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+function AlarmButton({ alarm, onClick, children }: AlarmButtonProps) {
+  return (
+    <AlarmButtonStyle alarm={alarm} onClick={onClick}>
+      {children}
+    </AlarmButtonStyle>
+  );
+}
+
+export default AlarmButton;
+
+const AlarmButtonStyle = styled.button<{ alarm: boolean }>`
+  width: 75px;
+  height: 32px;
+  background-color: ${({ theme, alarm }) =>
+    alarm ? theme.color_key : theme.color_bgLightOrange};
+  color: ${({ theme, alarm }) =>
+    alarm ? theme.color_textLightOrange : theme.color_key};
+  font-size: ${({ theme }) => theme.fontSize_xs};
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
