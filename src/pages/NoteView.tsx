ffa4903211abe8151c56949346567e6610c8a1a8
jinscoding <hojin9901@naver.com>
2024-08-14 08:38:04 +0900

    [Style/#29] NoteView 간격 조정

--- src/pages/NoteView.tsx
+++ src/pages/NoteView.tsx
@@ -50,7 +50,7 @@ function NoteView() {
 
   return (
     <NoteViewStyle showAllTags={showAllTags}>
-      <BackButton text='노트 보기' />
+      <BackButton text='노트보기' />
       <div className='container'>
         <div className='list'>
           <TagList
@@ -79,9 +79,9 @@ const NoteViewStyle = styled.div<{ showAllTags: boolean }>`
   padding: 24px 0;
 
   .container {
-    gap: 14px;
+    gap: 50px;
     display: grid;
-    grid-template-columns: 1fr 1.5fr;
+    grid-template-columns: 1fr 1.4fr;
     padding: 40px 0;
     position: relative;
 
