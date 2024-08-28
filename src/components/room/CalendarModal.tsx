abb53bee7ba222572707172b138ed42d1b517ee6
연하영 <dusgkdud7@naver.com>
2024-08-28 20:16:01 +0900

    [Fix/#73] 캘린더 모달 수정

--- src/components/room/CalendarModal.tsx
+++ src/components/room/CalendarModal.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Modal from '../common/Modal';
 import styled from 'styled-components';
 import NormalButton from '../common/NormalButton';
@@ -25,6 +25,7 @@ function CalendarModal({
   const [time, setTime] = useState('00:00');
   const [isAttendance, setIsAttendance] = useState(false);
   const { addSchedule, removeSchedule } = useSchedule(roomData.roomId);
+  const timeInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (isOpen) {
@@ -61,6 +62,12 @@ function CalendarModal({
     }
   };
 
+  const handleTimeClick = () => {
+    if (timeInputRef.current) {
+      timeInputRef.current.showPicker();
+    }
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <CalendarModalStyle>
@@ -80,12 +87,15 @@ function CalendarModal({
           </div>
           <div className='label-input'>
             <label>시간</label>
-            <input
-              type='time'
-              value={time}
-              onChange={(e) => setTime(e.target.value)}
-              disabled={!!schedule}
-            />
+            <div className='timewrapper' onClick={handleTimeClick}>
+              <input
+                ref={timeInputRef}
+                type='time'
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
+                disabled={!!schedule}
+              />
+            </div>
           </div>
         </div>
         <div className='bottom-bar'>
@@ -136,22 +146,29 @@ const CalendarModalStyle = styled.div`
   .label {
     display: flex;
     flex-direction: column;
+    align-items: center;
     gap: 15px;
     margin: 30px 0;
+    width: 100%;
 
     .label-input {
       display: flex;
       align-items: center;
       gap: 10px;
-      width: 100%;
+      width: 70%;
 
       label {
-        flex: 1;
+        flex: 0 0 50px;
         font-size: ${({ theme }) => theme.fontSize_sm};
       }
 
+      input,
+      .timewrapper {
+        flex: 1;
+        width: 100%;
+      }
+
       input {
-        width: 80%;
         outline: none;
         padding: 10px;
         border-radius: 8px;
@@ -193,6 +210,19 @@ const CalendarModalStyle = styled.div`
     color: ${({ theme }) => theme.color_textGray};
     text-align: left;
     margin: 10px 0 15px 0;
+  }
+
+  .timewrapper {
+    cursor: pointer;
+
+    input {
+      width: 100%;
+      outline: none;
+      padding: 10px;
+      border-radius: 8px;
+      border: 1px solid ${({ theme }) => theme.color_borderGray};
+      cursor: pointer;
+    }
   }
 `;
 
